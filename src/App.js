8bec1ee0d29cf0d9dce1d0c6261d8a3e04bf0c33
Daniel Bank <daniel.p.bank@gmail.com>
2021-04-28 21:36:39 -0700

    Adeles first app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function buttonClick(freq, offset = 1) {
+    var context = new AudioContext()
+    var o = context.createOscillator()
+    var  g = context.createGain()
+    o.connect(g)
+    o.frequency.value = freq
+    g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + offset)
+    g.connect(context.destination)
+    o.start(0)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Adele
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => { buttonClick(70, 1000)}}>FART</button>
+        <button onClick={() => { buttonClick(440)}}>CLICK ME</button>
+        <button onClick={() => { buttonClick(660)}}>CLICK ME</button>
+        <button onClick={() => { buttonClick(880)}}>CLICK ME</button>
       </header>
     </div>
   );
